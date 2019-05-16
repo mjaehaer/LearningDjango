ae4f0928e0fa02ac5dbd17c1d7917f94dde40874
mjaehaer <mjaehaer@users.noreply.github.com>
2019-05-16 22:36:14 +0300

    Master

--- LearningDjango/urls.py
+++ LearningDjango/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from econmap.views import Hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Hello, name='home')
 ]
