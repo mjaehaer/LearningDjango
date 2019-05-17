84138cbb7e019f7a69b630bffd7e1ff21da2ad5d
mjaehaer <mjaehaer@users.noreply.github.com>
2019-05-17 15:43:39 +0300

    Master

--- LearningDjango/urls.py
+++ LearningDjango/urls.py
@@ -17,11 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from econmap.views import Hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', Hello, name='home')
+    path('', include('econmap.urls')),
 ]
 
 if settings.DEBUG:
