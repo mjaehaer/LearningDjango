c9b3502d6b3e8858a53f8d2a6b95bc9d6cfa5e0f
mjaehaer <mjaehaer@users.noreply.github.com>
2019-05-17 21:23:39 +0300

    Master

--- econmap/views.py
+++ econmap/views.py
@@ -1,7 +1,14 @@
 
 from django.shortcuts import render
+from econmap.models import Category,Product
 
 # Create your views here.
 
 def base_view(request):
-    return render(request, 'base.html')
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    context = {
+        'categries': categories,
+        'products': products,
+    }
+    return render(request, 'base.html', context)
