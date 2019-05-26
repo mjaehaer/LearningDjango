4984d9e371a798ce6bd98c4a1d6fe2d412e4a479
mjaehaer <mjaehaer@users.noreply.github.com>
2019-05-26 17:58:49 +0300

    Master

--- econmap/views.py
+++ econmap/views.py
@@ -22,7 +22,9 @@ def product_view(request, product_slug):
 
 def category_view(request, category_slug):
     category = Category.objects.get(slug=category_slug)
+    products_of_category = Product.objects.filter(category=category)
     context = {
-        'category': category
+        'category': category,
+        'products_of_category': products_of_category
     }
     return render(request, 'category.html', context)
